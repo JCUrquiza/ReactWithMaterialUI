c3446bcb980a914a7b89aedf8571528d30082459
Juan <gurquiza8@gmail.com>
2023-05-27 23:16:02 -0500

    Creating a view page

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,13 @@
-import { MailOutline } from '@mui/icons-material';
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
-
-const drawerWidth = 240;
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
 
     return (
         <JournalLayout>
-            <Typography variant='h1' >Journal Page</Typography>
-            <MailOutline />
+
+            <NothingSelectedView />
+
         </JournalLayout>
         
     )
