c6d383515e3b016ac69651c07750b84c098986b5
Juan <gurquiza8@gmail.com>
2023-10-08 15:31:08 -0500

    Add form on the register page

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -2,40 +2,70 @@ import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Google } from '@mui/icons-material';
 import { Link as RouterLink } from 'react-router-dom';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+
+const formData = {
+    email: 'jc@gmail.com',
+    password: '123456',
+    displayName: 'Juan Urquiza'
+}
 
 export const RegisterPage = () => {
+
+    const { displayName, email, password, onInputChange, formState } = useForm(formData);
+
+    const onSubmit = ( event ) => {
+        event.preventDefault();
+        console.log(formState);
+    }
 
     return (
         
         <AuthLayout title='Register'>
 
-            <form>
+            <form onSubmit={ onSubmit }>
                 <Grid container>
                     <Grid item xs={ 12 } sx={{ mt: 2 }}>
                         <TextField 
                             label='Nombre completo'
                             type='text'
                             placeholder='John Doe'
-                            fullWidth />
+                            fullWidth
+                            name='displayName'
+                            value={displayName}
+                            onChange={onInputChange} 
+                            />
                     </Grid>
                     <Grid item xs={ 12 } sx={{ mt: 2 }}>
                         <TextField 
                             label='Correo'
                             type='email'
                             placeholder='correo@gmail.com'
-                            fullWidth />
+                            fullWidth
+                            name='email'
+                            value={email}
+                            onChange={onInputChange} 
+                        />
                     </Grid>
                     <Grid item xs={ 12 } sx={{ mt: 2 }}>
                         <TextField 
                             label='Contraseña'
                             type='password'
                             placeholder='Contraseña'
-                            fullWidth />
+                            fullWidth
+                            name='password'
+                            value={password}
+                            onChange={onInputChange} 
+                        />
                     </Grid>
 
                     <Grid container spacing={ 2 } sx={{ mb:2, mt: 1 }}>
                         <Grid item xs={ 12 } >
-                            <Button variant="contained" fullWidth>
+                            <Button 
+                                type='submit'
+                                variant="contained" 
+                                fullWidth
+                            >
                                 Crear cuenta
                             </Button>
                         </Grid>
